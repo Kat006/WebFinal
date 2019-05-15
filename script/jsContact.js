76c06b88cee77b8cc2dc6f0945620a08c1a65f72
Kat006 <43860472+Kat006@users.noreply.github.com>
2019-05-15 18:54:19 -0400

    contact reste

--- script/jsContact.js
+++ script/jsContact.js
@@ -1,30 +1,36 @@
 class NameForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
-
+    this.state = {nom: '', erreur: '', icon:{visibility: "hidden"}};
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
+  handleChange(e) {
+    this.setState({nom: e.target.value, erreur:'', icon:{visibility:"hidden"}});
+    if(!e.target.checkValidity() && e.target.value != ""){
+      e.target.style.boxShadow ="0px 0px 40px red";
+      this.setState({erreur:"Nom doit etre entre 3 et 25",icon: {visibility:"visible"}});
+    }else{
+      e.target.style.boxShadow ="";
+      this.setState({erreur:"",icon: {visibility:"hidden"}});
+    }
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <div>
         <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+          Nom:
+          </label>
+          <input type="text"
+          className="form-control"
+          id="nom"
+          placeholder="Votre Nom"
+          value={this.state.nom}
+          pattern="[a-zA-Z]{3,25}"
+          required
+          onChange={this.handleChange} />
+      </div>
     );
   }
 }
@@ -33,7 +39,60 @@ class MessageClient  extends React.Component {
   constructor(props){
     super(props);
     this.state = {message: '',erreur: '', icon: {visibility:"hidden"}};
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange(e){
+    this.setState({message: e.target.value});
+    if(!e.target.checkValidity() && e.target.value != ""){
+      e.target.style.boxShadow = "0px 0px 40px red";
+      this.setState({erreur:"Remplisez le message",icon: {visibility:"visible"}});
+    }else{
+      e.target.style.boxShadow = "";
+      this.setState({erreur:"",icon: {visibility:"hidden"}});
+    }
+  }
+
+  render = () =>
+    <div className = "ferm-group">
+      <label htmlFor="message">Votre commentaire <small className="text-danger"></small></label>
+      <textarea className="form-control"
+      id="message"
+      rows="3"
+      style={{boxShadow:"none"}}
+      required
+      value={this.state.message}
+      onChange={this.handleChange}
+      ></textarea>
+    </div>
 }
 
-ReactDOM.render( <NameForm />, document.getElementById("contact"));
+class Sexe extends React.Component {
+  constructor(props){
+    super(props);
+  }
+ render = () =>
+ <div className="form-group">
+  <label htmlFor="sexe"> Votre sexe</label>
+  <select id="sexe" className="form-control" defaultValue="Femme">
+    <option value="femme">Femme</option>
+    <option value="femme">Homme</option>
+    <option value="femme">Autre</option>
+  </select>
+  </div>
+  ;
+}
+
+
+class GererTout extends React.Component {
+  render = () =>
+      <div>
+        <NameForm />
+        <MessageClient />
+        <Sexe />
+      </div>
+      ;
+
+}
+
+ReactDOM.render( <GererTout />, document.getElementById("contact"));
